05b3ef34e630a6d46df0efaf2b5e1c9056ada258
Charlie Westaway <charleswestaway@gmail.com>
2021-04-30 15:43:57 -0400

    Updated Route for ForestFire Model

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import { Switch, Route } from 'react-router-dom';
   
 import Gallery from './Gallery/Gallery';
+import ForestFireContainer from './ForestFire/ForestFireContainer';
 import ErrorPage from './Error';
 
 export const App = () => (
 	<Switch>
 		<Route exact path='/' component={Gallery} />
-		<Route exact path='/forestfire' component={ErrorPage} />
+		<Route exact path='/forestfire' component={ForestFireContainer} />
+		<Route component={ErrorPage} />
 	</Switch>
 );
