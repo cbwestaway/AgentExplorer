22744265798aab9ecf2a2b01381aa9c02bdb1bfb
Emily Hu <emilyhu1224@gmail.com>
2021-05-01 17:03:09 -0400

    Name Changes

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import Typography from "@material-ui/core/Typography";
+import * as React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 const Error = () => (
   <Typography>Page Not Found :(</Typography>
--- src/components/Gallery/GalleryItem.tsx
+++ src/components/Gallery/GalleryItem.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
+import * as React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
-import { modelConfig } from "./gallery.interfaces";
+import { modelConfig } from './gallery.interfaces';
 
 
 const GalleryItem = (props: modelConfig) => {
